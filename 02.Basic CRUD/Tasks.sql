9445b76b8376e45983a25e5a9607589ae5e58f27
Gandjurov <gandjurov@abv.bg>
2019-01-21 00:16:40 +0200

    Temporary Save

--- 02.Basic CRUD/Tasks.sql
+++ 02.Basic CRUD/Tasks.sql
@@ -38,17 +38,39 @@ SELECT FirstName + ' ' + MiddleName + ' ' + LastName AS [Full Name]
  WHERE Salary IN (25000,14000,12500,23600) 
 
 -- 11. Find All Employees Without Manager 
-
+SELECT FirstName, LastName
+  FROM Employees
+ WHERE ManagerID IS NULL
+  
 -- 12. Find All Employees with Salary More Than 
+SELECT FirstName, LastName, Salary
+  FROM Employees
+ WHERE Salary > 50000
+ORDER BY Salary DESC
 
 -- 13. Find 5 Best Paid Employees 
+  SELECT TOP(5) FirstName, LastName
+    FROM Employees
+ORDER BY Salary DESC
 
 -- 14. Find All Employees Except Marketing
+  SELECT FirstName, LastName
+    FROM Employees
+   WHERE DepartmentID <> 4
 
 -- 15. Sort Employees Table 
+SELECT *
+  FROM Employees
+ORDER BY Salary DESC, FirstName, LastName DESC, MiddleName
+GO
 
 -- 16. Create View Employees with Salaries 
+CREATE VIEW V_EmployeesSalaries AS
+SELECT FirstName, LastName, Salary
+  FROM Employees
 
+SELECT * FROM V_EmployeesSalaries
+GO
 -- 17. Create View Employees with Job Titles 
 
 -- 18. Distinct Job Titles 
