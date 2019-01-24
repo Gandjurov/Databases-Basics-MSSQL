80783289c8349ae37cf452d5206e4ea2fb124ff0
Gandjurov <gandjurov@abv.bg>
2019-01-24 21:06:11 +0200

    Final Update

--- 03.Built-in Functions/Built-In Tasks.sql
+++ 03.Built-in Functions/Built-In Tasks.sql
@@ -122,3 +122,26 @@ ORDER BY Name, Duration, [Part of the Day]
 --18. Orders Table
 USE Orders
 
+SELECT ProductName, OrderDate, DATEADD(DAY, 3, OrderDate) AS [Pay Due],
+							   DATEADD(MONTH, 1, OrderDate) AS [Deliver Due]
+  FROM Orders
+
+--19. People Table
+CREATE TABLE People (
+	Id INT PRIMARY KEY IDENTITY,
+	Name NVARCHAR(50) NOT NULL,
+	Birthdate DATETIME NOT NULL
+)
+
+INSERT INTO People VALUES
+('Viktor', '2000-12-07'),
+('Steven', '1992-09-10'),
+('Stephen', '1910-09-19'),
+('John', '2010-01-06')
+
+SELECT Name,
+	DATEDIFF(YEAR, Birthdate, GETDATE()) AS [Age in Years],
+	DATEDIFF(MONTH, Birthdate, GETDATE()) AS [Age in Months],
+	DATEDIFF(DAY, Birthdate, GETDATE()) AS [Age in Days],
+	DATEDIFF(MINUTE, Birthdate, GETDATE()) AS [Age in Minutes]
+FROM People
