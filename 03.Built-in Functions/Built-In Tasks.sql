8977be6b4a410be75ca967cac272270991cce801
Gandjurov <gandjurov@abv.bg>
2019-01-24 20:40:49 +0200

    Add Orders.sql

--- 03.Built-in Functions/Built-In Tasks.sql
+++ 03.Built-in Functions/Built-In Tasks.sql
@@ -52,10 +52,22 @@ SELECT FirstName, LastName
  WHERE LEN(LastName) = 5
 
 --10. Rank Employees by Salary 
-
+  SELECT EmployeeID, FirstName, LastName, Salary,
+   DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS Rank
+    FROM Employees
+   WHERE Salary BETWEEN 10000 AND 50000
+ORDER BY Salary DESC
 
 --11. Find All Employees with Rank 2
-
+SELECT *
+  FROM (
+    SELECT EmployeeID, FirstName, LastName, Salary,
+   DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS Rank
+    FROM Employees
+   WHERE Salary BETWEEN 10000 AND 50000
+   ) AS MySpecialTable
+   WHERE MySpecialTable.Rank = 2
+   ORDER BY Salary DESC
 
 --12. Countries Holding 'A'
 USE Geography
@@ -66,18 +78,47 @@ USE Geography
 ORDER BY IsoCode
 
 --13. Mix of Peak and River Names 
+SELECT PeakName, RiverName, LOWER(PeakName + SUBSTRING(RiverName, 2, LEN(RiverName))) AS Mix
+  FROM Peaks, Rivers
+ WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
+ORDER BY PeakName, RiverName
 
 
 --14. Games From 2011 and 2012 Year
+USE Diablo
 
+  SELECT TOP(50) [Name], FORMAT([Start], 'yyyy-MM-dd', 'en-US') AS [Start]
+    FROM Games
+   WHERE YEAR(Start) BETWEEN 2011 AND 2012
+ORDER BY [Start], [Name]
 
 --15. User Email Providers 
-
+SELECT UserName, SUBSTRING(Email, CHARINDEX('@', Email) + 1, LEN(Email)) AS [Email Provider]
+  FROM Users
+ORDER BY [Email Provider], Username
 
 --16. Get Users with IPAddress Like Pattern 
-
+  SELECT UserName, IpAddress
+    FROM Users
+   WHERE IpAddress LIKE '___.1_%._%.___'
+ORDER BY UserName
 
 --17. Show All Games with Duration
-
+SELECT Name,
+  CASE 
+  WHEN DATEPART(HOUR, Start) >= 0 AND DATEPART(HOUR, Start) < 12 THEN 'Morning'
+  WHEN DATEPART(HOUR, Start) >= 12 AND DATEPART(HOUR, Start) < 18 THEN 'Afternoon'
+  WHEN DATEPART(HOUR, Start) >= 18 AND DATEPART(HOUR, Start) < 24 THEN 'Evening'
+  END AS [Part of the Day],
+  CASE
+  WHEN Duration <= 3 THEN 'Extra Short'
+  WHEN Duration BETWEEN 4 AND 6 THEN 'Short'
+  WHEN Duration >= 6 THEN 'Long'
+  WHEN Duration IS NULL THEN 'Extra Long'
+  END AS Duration
+  FROM Games
+ORDER BY Name, Duration, [Part of the Day]
 
 --18. Orders Table
+USE Orders
+
