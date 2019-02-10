b1b9e47aad9e8a4e6972fa2e81c9e0ecca4802d7
Gandjurov <gandjurov@abv.bg>
2019-02-10 21:10:33 +0200

    Add Data Manipolation Part

--- 09. Exam Preparation/Exam - 29 Aug 2018/Queries.sql
+++ 09. Exam Preparation/Exam - 29 Aug 2018/Queries.sql
@@ -1,3 +1,5 @@
+-- DDL PART --
+
 --CREATE DATABASE Supermarket
 
 --USE Supermarket
@@ -53,3 +55,44 @@ CREATE TABLE Shifts
 
 ALTER TABLE Shifts
 ADD CONSTRAINT CH_CheckInCheckOut CHECK(CheckIn < CheckOut)
+
+--DML PART --
+--INSERTs
+INSERT INTO Employees (FirstName, LastName, Phone, Salary) VALUES
+('Stoyan', 'Petrov', '888-785-8573', 500.25),
+('Stamat', 'Nikolov', '789-613-1122', 999995.25),
+('Evgeni', 'Petkov', '645-369-9517', 1234.51),
+('Krasimir', 'Vidolov',	'321-471-9982',	50.25)
+
+INSERT INTO Items ([Name], Price, CategoryId) VALUES
+('Tesla battery', 154.25, 8),
+('Chess', 30.25, 8),
+('Juice', 5.32,	1),
+('Glasses',	10,	8),
+('Bottle of water',	1, 1)
+
+--UPDATE
+--Make all items’ prices 27% more expensive where the category ID is either 1, 2 or 3.
+UPDATE Items
+   SET Price *= 1.27
+ WHERE CategoryId IN(1,2,3)
+
+--check how records will be affected
+--SELECT COUNT(*)
+--  FROM Items
+-- WHERE CategoryId IN(1,2,3)
+
+--DELETE
+--Delete all order items where the order id is 48 (be careful with the relationships)
+DELETE FROM OrderItems
+WHERE OrderId = 48
+
+DELETE FROM Orders
+WHERE Id = 48
+
+
+--Querying
+
+
+
+
