9a5cdff3e573b4024771efd318d14a670582cb9d
Gandjurov <gandjurov@abv.bg>
2019-02-12 14:09:46 +0200

    add more exercises

--- 09. Exam Preparation/Exam - 29 Aug 2018/Queries.sql
+++ 09. Exam Preparation/Exam - 29 Aug 2018/Queries.sql
@@ -127,5 +127,46 @@ ORDER BY [Work hours] DESC, e.Id
 GO
 
 --9. The Most Expensive Order
+SELECT TOP 1 oi.OrderId, SUM(i.Price * oi.Quantity) AS TotalPrice
+  FROM OrderItems AS oi 
+  JOIN Items AS i ON i.Id = oi.ItemId
+GROUP BY oi.OrderId
+ORDER BY TotalPrice DESC
+GO
 
+--10. Rich Item, Poor Item 
+SELECT TOP(10) oi.OrderId
+		, MAX(i.Price) AS ExpensivePrice
+		, MIN(i.Price) AS CheapPrice
+  FROM OrderItems AS oi 
+  JOIN Items AS i ON i.Id = oi.ItemId
+GROUP BY oi.OrderId
+ORDER BY ExpensivePrice DESC, oi.OrderId
+GO
 
+--11. Cashiers
+SELECT DISTINCT e.Id, FirstName, LastName
+  FROM Employees AS e
+  JOIN Orders AS o ON o.EmployeeId = e.Id
+ORDER BY e.Id
+GO
+
+--12. Lazy Employees 
+SELECT DISTINCT e.Id, 
+       CONCAT(e.FirstName, ' ', e.LastName) AS [Full Name]
+  FROM Employees AS e
+  JOIN Shifts AS s ON s.EmployeeId = e.Id
+ WHERE DATEDIFF(HOUR, s.CheckIn, s.CheckOut) < 4
+ORDER BY e.Id
+
+--13. Sellers 
+SELECT TOP(10) CONCAT(e.FirstName, ' ', e.LastName) AS [Full Name],
+	   SUM(i.Price * oi.Quantity) AS [Total Price],
+	   SUM(oi.Quantity) AS [Items]
+  FROM Employees AS e
+  JOIN Orders AS o ON o.EmployeeId = e.Id
+  JOIN OrderItems AS oi ON oi.OrderId = o.Id
+  JOIN Items AS i ON i.Id = oi.ItemId
+ WHERE o.DateTime < '2018-06-15'
+GROUP BY e.FirstName, e.LastName
+ORDER BY [Total Price] DESC, [Items] DESC
