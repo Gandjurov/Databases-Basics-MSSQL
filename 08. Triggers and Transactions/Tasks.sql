f76c196d988161f91cdd144f09fce8c24d505440
Gandjurov <gandjurov@abv.bg>
2019-02-13 15:27:57 +0200

    Add exercises

--- 08. Triggers and Transactions/Tasks.sql
+++ 08. Triggers and Transactions/Tasks.sql
@@ -54,11 +54,70 @@ INSERT INTO Accounts (Id, AccountHolderId, Balance) VALUES (17, 11, 76653.20);
 INSERT INTO Accounts (Id, AccountHolderId, Balance) VALUES (18, 1, 235469.89);
 
 --14. Create Table Logs 
+CREATE TABLE Logs
+(
+	LogId INT PRIMARY KEY IDENTITY,
+	AccountId INT FOREIGN KEY REFERENCES Accounts(Id),
+	OldSum DECIMAL(15,2),
+	NewSum DECIMAL(15,2)
+)
+GO
 
+CREATE TRIGGER tr_InsertAccountInfo ON Accounts FOR UPDATE
+AS
+DECLARE @newSum DECIMAL(15,2) = (SELECT Balance FROM inserted)
+DECLARE @oldSum DECIMAL(15,2) = (SELECT Balance FROM deleted)
+DECLARE @accountId INT = (SELECT Id FROM inserted)
 
+INSERT INTO Logs (AccountId, NewSum, OldSum) VALUES
+(
+	@accountId,
+	@newSum,
+	@oldSum
+)
+GO
+
+UPDATE Accounts
+SET Balance += 10
+WHERE Id = 1
+
+SELECT *
+  FROM Accounts WHERE Id = 1
+
+SELECT *
+  FROM Logs
+GO
 --15. Create Table Emails 
+CREATE TABLE NotificationEmails
+( 
+   Id INT PRIMARY KEY IDENTITY
+  ,Recipient INT FOREIGN KEY REFERENCES Accounts(Id)
+  ,Subject VARCHAR(50)
+  ,Body VARCHAR(MAX)
+)
+GO
 
+CREATE TRIGGER tr_LogEMail ON Logs FOR INSERT
+AS
+DECLARE @accountId INT = (SELECT TOP(1) AccountId FROM inserted)
+DECLARE @oldSum DECIMAL(15,2) = (SELECT TOP(1) OldSum FROM inserted)
+DECLARE @newSum DECIMAL(15,2) = (SELECT TOP(1) NewSum FROM inserted)
 
+INSERT INTO NotificationEmails(Recipient, Subject, Body) VALUES
+(
+@accountId,
+'Balance change for account: ' + CAST(@accountId AS VARCHAR(20)),
+'On ' + CONVERT(VARCHAR(30), GETDATE(), 103) + ' your balance was changed from ' + 
+CAST(@oldSum AS VARCHAR(20)) + ' to ' + CAST(@newSum AS VARCHAR(20))
+)
+
+SELECT * 
+FROM Accounts
+WHERE Id = 1
+
+UPDATE Accounts
+SET Balance += 100
+WHERE Id = 1
 --16. Deposit Money 
 
 
