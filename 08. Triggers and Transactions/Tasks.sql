cb4aeefa64d6f949547f14e7044e0d97b0ceda4c
Gandjurov <gandjurov@abv.bg>
2019-02-13 22:43:04 +0200

    Final update

--- 08. Triggers and Transactions/Tasks.sql
+++ 08. Triggers and Transactions/Tasks.sql
@@ -347,3 +347,19 @@ BEGIN
 END 
 
 --22. Delete Employees 
+
+CREATE TABLE Deleted_Employees
+(
+	EmployeeId INT PRIMARY KEY IDENTITY,
+	FirstName VARCHAR(50) NOT NULL,
+	LastName VARCHAR(50) NOT NULL,
+	MiddleName VARCHAR(50),
+	JobTitle VARCHAR(50) NOT NULL,
+	DepartmentID INT NOT NULL,
+	Salary MONEY NOT NULL
+)
+GO
+
+CREATE TRIGGER tr_DeleteEmployees ON Employees AFTER DELETE AS
+	INSERT INTO Deleted_Employees
+SELECT FirstName, LastName, MiddleName, JobTitle, DepartmentID, Salary FROM deleted
