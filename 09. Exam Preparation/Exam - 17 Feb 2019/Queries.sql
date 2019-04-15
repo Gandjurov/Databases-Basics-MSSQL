a2c5a228eba51111eaaaa746be0b0ecf07646f8e
Gandjurov <gandjurov@abv.bg>
2019-04-15 12:26:31 +0300

    Add 16. Average Grade per Subject and 17. Exams Information Queries

--- 09. Exam Preparation/Exam - 17 Feb 2019/Queries.sql
+++ 09. Exam Preparation/Exam - 17 Feb 2019/Queries.sql
@@ -185,8 +185,31 @@ GROUP BY t.FirstName, t.LastName, s.FirstName, s.LastName, su.Name
 ORDER BY j.SubjectName,j.[Teacher Full Name], TopGrade DESC
 
 --16. Average Grade per Subject
+SELECT s.Name, AVG(ss.Grade) AS AverageGrade
+  FROM Subjects AS s
+  JOIN StudentsSubjects AS ss ON ss.SubjectId = s.Id
+GROUP BY s.Name, s.Id
+ORDER BY s.Id
 
 --17. Exams Information
+SELECT  k.Quarter, k.SubjectName, COUNT(k.StudentId) AS StudentsCount
+  FROM (
+  SELECT s.Name AS SubjectName,
+		 se.StudentId,
+		 CASE
+		 WHEN DATEPART(MONTH, Date) BETWEEN 1 AND 3 THEN 'Q1'
+		 WHEN DATEPART(MONTH, Date) BETWEEN 4 AND 6 THEN 'Q2'
+		 WHEN DATEPART(MONTH, Date) BETWEEN 7 AND 9 THEN 'Q3'
+		 WHEN DATEPART(MONTH, Date) BETWEEN 10 AND 12 THEN 'Q4'
+		 WHEN Date IS NULL THEN 'TBA'
+		 END AS [Quarter]
+    FROM Exams AS e
+	JOIN Subjects AS s ON s.Id = e.SubjectId 
+	JOIN StudentsExams AS se ON se.ExamId = e.Id
+	WHERE se.Grade >= 4
+) AS k
+GROUP BY k.Quarter, k.SubjectName
+ORDER BY k.Quarter
 
 --18. Exam Grades
 
